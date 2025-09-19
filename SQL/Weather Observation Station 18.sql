aff1b165235bd04ead66d0b7b2237b76df54eb48
Dusan Vilicic Held <dvheld@gmail.com>
2025-09-19 20:17:15 -0300

    Create Weather Observation Station 18.sql

---
+++ SQL/Weather Observation Station 18.sql
@@ -0,0 +1,8 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-18/problem */
+
+SELECT
+    CAST(
+        MAX(LAT_N) - MIN(LAT_N) +
+        MAX(LONG_W) - MIN(LONG_W)
+    AS DECIMAL(10,4))
+FROM STATION;
