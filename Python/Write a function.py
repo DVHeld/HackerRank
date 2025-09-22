5d89ec22a20cd5459b230eac809682821f4c123b
Dusan Vilicic Held <dvheld@gmail.com>
2025-09-22 16:50:31 -0300

    Create Write a function.py

---
+++ Python/Write a function.py
@@ -0,0 +1,16 @@
+"""Write a function
+
+    https://www.hackerrank.com/challenges/write-a-function/problem
+    """
+
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            return False
+        return True
+    return False
+
+year = int(input())
+print(is_leap(year))
