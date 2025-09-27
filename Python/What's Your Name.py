7c5abb1911854d246661d26b6d925f9641b0b502
Dusan Vilicic Held <dvheld@gmail.com>
2025-09-27 09:34:16 -0300

    Create What's Your Name.py

---
+++ Python/What's Your Name.py
@@ -0,0 +1,19 @@
+"""What's Your Name?
+
+    https://www.hackerrank.com/challenges/whats-your-name/problem
+    """
+
+def print_full_name(first, last) -> str:
+    """Prints a greeting for the given person.
+
+    Args:
+        first (str): The first name.
+        last (str): The last name.
+    """
+
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
